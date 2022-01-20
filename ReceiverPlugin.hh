44cff3315fa67196d2d480b8c0d3d71bfc24012b
ciaran13 <ciaran@helgen.tech>
2022-01-20 12:56:43 +0000

    Cleanup comments

--- ReceiverPlugin.hh
+++ ReceiverPlugin.hh
@@ -8,29 +8,25 @@
 
 namespace gazebo
 {
-  /// \brief An example plugin for a receiver sensor.
   class ReceiverPlugin : public SensorPlugin
   {
-    /// \brief Constructor.
     public: ReceiverPlugin();
 
-    /// \brief Destructor.
     public: virtual ~ReceiverPlugin();
 
-    /// \brief Load the sensor plugin.
-    /// \param[in] _sensor Pointer to the sensor that loaded this plugin.
-    /// \param[in] _sdf SDF element that describes the plugin.
+    // Load the sensor plugin.
+    // _sensor Pointer to the sensor that loaded this plugin.
+    // _sdf SDF element that describes the plugin.
     public: virtual void Load(sensors::SensorPtr _sensor, sdf::ElementPtr _sdf);
 
-    /// \brief Callback that receives the receiver sensor's update signal.
+    // Callback that receives the receiver sensor's update signal.
     private: virtual void OnUpdate();
-    //private: virtual bool UpdateImpl();
 
-    /// \brief Pointer to the receiver sensor
+    // Pointer to the receiver sensor
     private: sensors::WirelessReceiverPtr parentSensor;
 
-    /// \brief Connection that maintains a link between the receiver sensor's
-    /// updated signal and the OnUpdate callback.
+    // Connection that maintains a link between the receiver sensor's
+    // updated signal and the OnUpdate callback.
     private: event::ConnectionPtr updateConnection;
   };
 }
